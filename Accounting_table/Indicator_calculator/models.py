bf4307fd9d7e099d95759b991b542baacee5a078
ChashnikovEvgeni <chashnikov.as1804@asugubkin.ru>
2021-11-28 02:16:07 +0300

    Initial commit

---
+++ Accounting_table/Indicator_calculator/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=255)
+    target_indicator_IUC = models.DecimalField(max_digits=5, decimal_places=2)
+    actual_indicator_IUC = models.DecimalField(max_digits=5, decimal_places=2)
+    # list_of_departments
+
+class Department(models.Model):
+    title = models.TextField()
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    #List_of_indicators
+    #Уточнить название перемнной, фактический или плановый
+    PPRTD_weight = models.DecimalField(max_digits=5, decimal_places=4)
+    PFVIR_weight = models.DecimalField(max_digits=5, decimal_places=4)
+    department_indicator = models.DecimalField(max_digits=4, decimal_places=1)
+
+class Indicator(models.Model):
+    title = models.CharField(max_length=255)
+    units = models.CharField(max_length=255)
+    comment = models.TextField()
+    target_indicator = models.DecimalField(max_digits=4, decimal_places=1)
+    actual_indicator = models.DecimalField(max_digits=4, decimal_places=1)
+    Significance_of_indicator = models.DecimalField(max_digits=5, decimal_places=4)
+    Plan = models.DecimalField(max_digits=4, decimal_places=1)
+    Degree_of_compliance = models.DecimalField(max_digits=4, decimal_places=1)
